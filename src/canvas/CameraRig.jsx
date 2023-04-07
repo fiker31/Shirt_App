3783c502bd77e6039496d754466a28bd8f942e23
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-04-07 12:30:20 +0300

    camerarig

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React , {useRef} from 'react';
+import {useFrame} from '@react-three/fiber';
+import {easing} from 'maath';
+import { useSnapshot } from 'valtio';
+import state from '../store';
 
-const CameraRig = () => {
-  return (
-    <div>CameraRig</div>
-  )
+
+const CameraRig = ({childern}) => {
+const group=useRef();
+const snap=useSnapshot(state);
+useFrame ((state,delta)=>{
+  const isBreakpoint= window.innerWidth<=1260;
+  const isMobile=window.innerWidth<=600;
+})
+
+
 }
 
 export default CameraRig
--- src/store/state.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const state = () => {
-  return (
-    <div>state</div>
-  )
-}
-
-export default state

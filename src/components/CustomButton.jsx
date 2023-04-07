8a69035738cd910f6ead337685102e902fe068ff
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-04-07 11:09:15 +0300

    customizer

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useSnapshot } from 'valtio';
 
 import state from '../store';
-import { getContrastingColor } from '../config/helpers';
 
 const CustomButton = ({ type, title, customStyles, handleClick }) => {
   const snap = useSnapshot(state);
@@ -21,7 +20,6 @@ const CustomButton = ({ type, title, customStyles, handleClick }) => {
       }
     }
   }
-
   return (
     <button
       className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}

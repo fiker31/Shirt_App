4e52a841bbf369255204000c0387de3853f40456
Fiker Yibeltal <Fikeryibeltal31@gmail.com>
2023-04-07 12:16:03 +0300

    shirt

--- src/canvas/Shirt.jsx
+++ src/canvas/Shirt.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {easing} from 'maath';
 import { useSnapshot } from 'valtio';
-import {useFrame} from '@react-three/fiber'
+import {useFrame} from '@react-three/fiber';
 import { Decal,useGLTF,useTexture } from '@react-three/drei';
 import state from '../store';
 
